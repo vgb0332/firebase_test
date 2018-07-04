44eaf7d3eaae580e63b36004fa15efc4deccd00f
mjung <vgb000123@gmail.com>
2018-07-04 22:50:32 +0900

    i

--- public/js/calendarInit.js
+++ public/js/calendarInit.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
     themeSystem: 'standard',
     defaultView: 'agenda',
     groupByResource: true,
-    visibleRange: {
-      start: '2018-06-30',
-      end: moment().add('1', 'day'),
-    },
+    // visibleRange: {
+    //   start: moment(),
+    //   end: moment().add('1', 'day'),
+    // },
     resources: [
       { id: '01062610332', title: '정용석' },
     ],
@@ -67,6 +67,17 @@ $(document).ready(function() {
     viewRender: function(view) {
         var title = view.title;
         $("#title").html( title );
+        console.log(calendar.fullCalendar( 'clientEvents' ));
+        var events = calendar.fullCalendar( 'clientEvents' );
+        var totTime = 0;
+        $.each(events, function( index, event ) {
+          console.log(event);
+          var diff = moment.duration( event.end.diff(event.start) ).asMinutes();
+          totTime+= diff;
+        });
+
+        $("#spent-time").html(totTime);
+        $("#spent-time-hm").html(Math.floor(totTime / 60).toFixed(0) + '시간 ' + totTime % 60 + '분');
   	},
     views: {
       oneday: {
@@ -86,10 +97,28 @@ $(document).ready(function() {
       $('#eventBlockModal').on('show.bs.modal', function () {
         var startDate = event.start.format('a h:mm');
         var endDate = event.end.format('a h:mm');
+        console.log(event);
+        $("#contentEditButton, #locationEditButton, #emotionEditButton").removeClass('active');
+
+        $("#eventContent").attr('contenteditable', false);
+        $('#locationContent input[type=radio]').prop('disabled', true);
+        $('#happyContent input[type=radio]').prop('disabled', true);
+        $('#satisfactionContent input[type=radio]').prop('disabled', true);
+        $('#depressionContent input[type=radio]').prop('disabled', true);
+        $('#anxietyContent input[type=radio]').prop('disabled', true);
+        $('#angerContent input[type=radio]').prop('disabled', true);
+        $('#fatigueContent input[type=radio]').prop('disabled', true);
+
         $("#eventBlockModal .modal-title").text(startDate + ' - ' + endDate);
         $('#eventBlockModal #eventContent').text(event.title);
-        $('#eventBlockModal #emotionContent').text(event.content.emotion);
-        $('#eventBlockModal #locationContent').text(event.content.location);
+
+        $('#eventBlockModal #locationContent input[type=radio][value=' + event.content.location + ']').attr("checked", true);
+        $('#eventBlockModal #happyContent input[type=radio][value=' + event.content.happy + ']').attr("checked", true);
+        $('#eventBlockModal #satisfactionContent input[type=radio][value=' + event.content.satisfaction + ']').attr("checked", true);
+        $('#eventBlockModal #depressionContent input[type=radio][value=' + event.content.depression + ']').attr("checked", true);
+        $('#eventBlockModal #anxietyContent input[type=radio][value=' + event.content.anxiety + ']').attr("checked", true);
+        $('#eventBlockModal #angerContent input[type=radio][value=' + event.content.anger + ']').attr("checked", true);
+        $('#eventBlockModal #fatigueContent input[type=radio][value=' + event.content.fatigue + ']').attr("checked", true);
       });
 
       $("#eventBlockModal").modal();
@@ -116,22 +145,19 @@ $(document).ready(function() {
         $("#cancelAddBlockButton").click(function(e) {
           console.log('cancel clicked');
           $("#cancelAddBlockButton, #addBlockButton").off('click');
+          $("#addButton").removeClass('active');
           calendar.fullCalendar('option', 'selectable', false);
         });
 
         $("#addBlockButton").click( function(e) {
-          var emotion = $("#addBlockModal #emotionSel").val();
-          var location = $("#addBlockModal #locationSel").val();
+          var location = $("#addBlockModal #locationSel input[type=radio]:checked").val();
           var title = $("#addContent").val();
-
-          if(emotion === '감정'){
-            alert('감정을 선택해주세요!');
-            return false;
-          }
-          if(location === '장소'){
-            alert('장소를 선택해주세요!');
-            return false;
-          }
+          var happy = $("#addBlockModal #happySel input[type=radio]:checked").val();
+          var satisfaction = $("#addBlockModal #satisfactionSel input[type=radio]:checked").val();
+          var depression = $("#addBlockModal #depressionSel input[type=radio]:checked").val();
+          var anxiety = $("#addBlockModal #anxietySel input[type=radio]:checked").val();
+          var anger = $("#addBlockModal #angerSel input[type=radio]:checked").val();
+          var fatigue = $("#addBlockModal #fatigueSel input[type=radio]:checked").val();
 
           if( confirm('추가 하시겠습니까?') ){
               calendar.fullCalendar( 'renderEvent', {
@@ -139,8 +165,14 @@ $(document).ready(function() {
                 resourceId: '01062610332',
                 title: title,
                 content: {
-                  emotion : emotion,
+                  title: title,
                   location : location,
+                  happy: happy,
+                  satisfaction: satisfaction,
+                  depression : depression,
+                  anxiety: anxiety,
+                  anger: anger,
+                  fatigue: fatigue,
                 },
                 start: moment(start),
                 end: moment(end),
@@ -154,7 +186,7 @@ $(document).ready(function() {
           console.log(calendar.fullCalendar( 'clientEvents' ))
           $("#cancelAddBlockButton, #addBlockButton").off('click');
           calendar.fullCalendar('option', 'selectable', false);
-
+          $("#addButton").removeClass('active');
         });
       }
       else{
@@ -168,52 +200,59 @@ $(document).ready(function() {
     snapDuration: "00:10:00",
   });
 
-  $('#emotionSel').mobileSelect({
-    title: '감정 수준',
-    buttonSave: '확인',
-    buttonCancel: '취소',
-    animation: 'scale',
-    animationSpeed: 400,
-    onClose: function(){
-        $("#emotionContent").html($(this).val());
-    },
-  });
-
-  $('#locationSel').mobileSelect({
-    title: '장소',
-    buttonSave: '확인',
-    buttonCancel: '취소',
-    animation: 'scale',
-    animationSpeed: 100,
-    onClose: function(){
-        $("#locationContent").html($(this).val());
-    },
-  });
+  // $('#emotionSel').mobileSelect({
+  //   title: '감정 수준',
+  //   buttonSave: '확인',
+  //   buttonCancel: '취소',
+  //   animation: 'scale',
+  //   animationSpeed: 400,
+  //   onClose: function(){
+  //       $("#emotionContent").html($(this).val());
+  //   },
+  // });
+  //
+  // $('#locationSel').mobileSelect({
+  //   title: '장소',
+  //   buttonSave: '확인',
+  //   buttonCancel: '취소',
+  //   animation: 'scale',
+  //   animationSpeed: 100,
+  //   onClose: function(){
+  //       $("#locationContent").html($(this).val());
+  //   },
+  // });
 
   $("#dayDisplay").click( function(e) {
     calendar.fullCalendar('changeView', 'oneday');
-    calendar.fullCalendar( 'gotoDate', moment('2018-06-27') );
+    calendar.fullCalendar( 'gotoDate', moment() );
   });
 
   $("#nextdayDisplay").click( function(e) {
     calendar.fullCalendar('changeView', 'oneday');
-    calendar.fullCalendar( 'gotoDate', moment('2018-06-28') );
+    calendar.fullCalendar( 'gotoDate', moment().add('1', 'day') );
   });
 
   $("#bothdayDisplay").click( function(e) {
     calendar.fullCalendar('changeView', 'twoday');
-    calendar.fullCalendar( 'gotoDate', moment('2018-06-27') );
+    calendar.fullCalendar( 'gotoDate', moment() );
   });
 
   $("#addButton").click( function(e) {
     console.log('add button click');
-    alert('시간을 선택해주세요(클릭 혹은 드래그)');
-    calendar.fullCalendar('option', 'selectable', true);
+
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      calendar.fullCalendar('option', 'selectable', false);
+
+    }
+    else{
+      $(this).addClass('active');
+      calendar.fullCalendar('option', 'selectable', true);
+      alert('시간을 선택해주세요(클릭 혹은 드래그)');
+    }
   });
 
   $("#currentButton").click( function(e) {
-    console.log($('.fc-now-indicator').position().top);
-    console.log($(window).height() / 2);
     // $(".fc-scroller.fc-time-grid-container").scrollTop($('.fc-now-indicator').position().top - $(window).height() / 2);
     $(".fc-scroller.fc-time-grid-container").animate({
       scrollTop: $('.fc-now-indicator').position().top - $(window).height() / 2
@@ -222,6 +261,7 @@ $(document).ready(function() {
 
   //initialize current time for the title
   $("#time").html( moment().format("a h:mm") );
+
   setInterval(function(){
     console.log('rendering every minute');
     var time = moment().format("a h:mm");
@@ -229,28 +269,71 @@ $(document).ready(function() {
   },60000);
 
   $("#contentEditButton").click( function(e) {
-    $("#eventContent").attr('contenteditable', true).focus();
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $("#eventContent").attr('contenteditable', false);
+    }
+    else {
+      $(this).addClass('active');
+      $("#eventContent").attr('contenteditable', true).focus();
+    }
+
   });
 
   $("#emotionEditButton").click( function(e) {
-    $('#emotionSel').mobileSelect('show');
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $('#happyContent input[type=radio]').prop('disabled', true);
+      $('#satisfactionContent input[type=radio]').prop('disabled', true);
+      $('#depressionContent input[type=radio]').prop('disabled', true);
+      $('#anxietyContent input[type=radio]').prop('disabled', true);
+      $('#angerContent input[type=radio]').prop('disabled', true);
+      $('#fatigueContent input[type=radio]').prop('disabled', true);
+    }
+    else {
+      $(this).addClass('active');
+      $('#happyContent input[type=radio]').prop('disabled', false);
+      $('#satisfactionContent input[type=radio]').prop('disabled', false);
+      $('#depressionContent input[type=radio]').prop('disabled', false);
+      $('#anxietyContent input[type=radio]').prop('disabled', false);
+      $('#angerContent input[type=radio]').prop('disabled', false);
+      $('#fatigueContent input[type=radio]').prop('disabled', false);
+    }
+
   });
 
   $("#locationEditButton").click( function(e) {
-    $('#locationSel').mobileSelect('show');
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $('#locationContent input[type=radio]').prop('disabled', true);
+    }
+    else {
+      $(this).addClass('active');
+      $('#locationContent input[type=radio]').prop('disabled', false);
+    }
   });
 
   $("#updateBlockButton").click ( function(e) {
     if( confirm('수정하시겠습니까?')) {
-      var emotion = $("#emotionContent").text();
-      var location = $("#locationContent").text();
-      var content = $("#eventContent").text();
+      var location = $("#addBlockModal #locationSel input[type=radio]:checked").val();
+      var title = $("#eventContent").text();
+      var happy = $("#addBlockModal #happySel input[type=radio]:checked").val();
+      var satisfaction = $("#addBlockModal #satisfactionSel input[type=radio]:checked").val();
+      var depression = $("#addBlockModal #depressionSel input[type=radio]:checked").val();
+      var anxiety = $("#addBlockModal #anxietySel input[type=radio]:checked").val();
+      var anger = $("#addBlockModal #angerSel input[type=radio]:checked").val();
+      var fatigue = $("#addBlockModal #fatigueSel input[type=radio]:checked").val();
 
-      currentEvent.title = content;
-      currentEvent.content.emotion = emotion;
+      currentEvent.title = title;
+      currentEvent.content.title = title;
       currentEvent.content.location = location;
+      currentEvent.content.happy = happy;
+      currentEvent.content.satisfaction = satisfaction;
+      currentEvent.content.depression = depression;
+      currentEvent.content.anxiety = anxiety;
+      currentEvent.content.anger = anger;
+      currentEvent.content.fatigue = fatigue;
 
-      console.log(emotion, location, content, currentEvent);
       calendar.fullCalendar( 'updateEvent', currentEvent );
 
       $("#eventBlockModal").modal('hide');
